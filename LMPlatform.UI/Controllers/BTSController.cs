7b9941da8ef6941e214568415d205c0170d28eaf
infantika <infantika@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2013-11-11 19:51:50 +0000

    Реализация структуры баг-трекинговой системы
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@7 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/Controllers/BTSController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace LMPlatform.UI.Controllers
+{
+    public class BTSController : Controller
+    {
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectManagement()
+        {
+            return View();
+        }
+
+        public ActionResult ErrorManagement()
+        {
+            return View();
+        }
+    }
+}
