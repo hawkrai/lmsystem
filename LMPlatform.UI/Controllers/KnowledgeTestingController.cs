5dd9ca54d57662fc48f636a358e8f7c18c77ef04
girl10011 <girl10011@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2013-11-13 18:42:05 +0000

    Knowledge Testing module UI structure
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@9 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/Controllers/KnowledgeTestingController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Application.Core.UI.Controllers;
+
+namespace LMPlatform.UI.Controllers
+{
+    public class KnowledgeTestingController : BasicController
+    {
+        [Authorize, HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize, HttpGet]
+        public ActionResult Tests()
+        {
+            return View();
+        }
+
+        [Authorize, HttpGet]
+        public ActionResult TestResults()
+        {
+            return View();
+        }
+    }
+}
