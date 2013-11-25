a9bd7b1b08a77a04458639adef862669c75acbd9
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2013-11-25 21:24:58 +0000

    Added AdministrationViewModels
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@20 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/ViewModels/AdministrationViewModels/GroupViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using LMPlatform.Models;
+
+namespace LMPlatform.UI.ViewModels.AdministrationViewModels
+{
+  public class GroupViewModel
+  {
+    public int Id { get; set; }
+
+    [DisplayName("Номер")]
+    public string Name { get; set; }
+
+    [DisplayName("Год поступления")]
+    public string StartYear { get; set; }
+
+    [DisplayName("Год выпуска")]
+    public string GraduationYear { get; set; }
+
+    public static GroupViewModel FormGroup(Group group)
+    {
+      return new GroupViewModel
+      {
+        Id = group.Id,
+        Name = group.Name,
+        StartYear = group.StartYear,
+        GraduationYear = group.GraduationYear
+      };
+    }
+  }
+}
---
+++ LMPlatform.UI/ViewModels/AdministrationViewModels/StudentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using LMPlatform.Models;
+
+namespace LMPlatform.UI.ViewModels.AdministrationViewModels
+{
+  public class StudentViewModel
+  {
+    [DisplayName("Номер")]
+    public int Id { get; set; }
+
+    [DisplayName("Полное имя")]
+    public string FullName { get; set; }
+
+    [DisplayName("Номер группы")]
+    public string Group { get; set; }
+
+    public static StudentViewModel FromStudent(Student student)
+    {
+      return new StudentViewModel
+      {
+        Id = student.Id,
+        FullName = string.Format("{0} {1}", student.FirstName, student.LastName),
+        Group = student.Group.Name,
+      };
+    }
+  }
+}
