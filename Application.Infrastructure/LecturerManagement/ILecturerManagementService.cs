9556b79564a32ae3a6bd1650503ce0eaae1af251
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2013-12-08 11:40:22 +0000

    Added LecturerManagement
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@37 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ Application.Infrastructure/LecturerManagement/ILecturerManagementService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMPlatform.Models;
+
+namespace Application.Infrastructure.LecturerManagement
+{
+  public interface ILecturerManagementService
+  {
+    Lecturer GetLecturer(int userId);
+
+    List<Lecturer> GetLecturers();
+
+    void Save(Lecturer lecturer);
+
+    void UpdateLecturer(Lecturer lecturer);
+  }
+}
---
+++ Application.Infrastructure/LecturerManagement/LecturerManagementService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Data;
+using LMPlatform.Data.Repositories;
+using LMPlatform.Models;
+
+namespace Application.Infrastructure.LecturerManagement
+{
+  public class LecturerManagementService : ILecturerManagementService
+  {
+    public Lecturer GetLecturer(int userId)
+    {
+      using (var repositoriesContainer = new LmPlatformRepositoriesContainer())
+      {
+        return repositoriesContainer.LecturerRepository.GetBy(new Query<Lecturer>(e => e.Id == userId).Include(e => e.SubjectLecturers).Include(e => e.User));
+      }
+    }
+
+    public List<Lecturer> GetLecturers()
+    {
+      using (var repositoriesContainer = new LmPlatformRepositoriesContainer())
+      {
+        return repositoriesContainer.LecturerRepository.GetAll(new Query<Lecturer>().Include(e => e.SubjectLecturers)).ToList();
+      }
+    }
+
+    public void Save(Lecturer lecturer)
+    {
+      using (var repositoriesContainer = new LmPlatformRepositoriesContainer())
+      {
+        repositoriesContainer.LecturerRepository.SaveLecturer(lecturer);
+        repositoriesContainer.ApplyChanges();
+      }
+    }
+
+    public void UpdateLecturer(Lecturer lecturer)
+    {
+      using (var repositoriesContainer = new LmPlatformRepositoriesContainer())
+      {
+        repositoriesContainer.LecturerRepository.Save(lecturer);
+        repositoriesContainer.ApplyChanges();
+      }
+    }
+  }
+}
