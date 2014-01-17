b3c7d6719b7485b9af761afbe27bd0c0bb2d93ff
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-01-17 13:12:37 +0000

    Administration UI changes
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@51 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- LMPlatform.UI/Scripts/application/Administration/administrationManagement.js
+++ LMPlatform.UI/Scripts/application/Administration/administrationManagement.js
@@ -6,38 +6,16 @@
 
 ////functions
 function initStudentManagement() {
-    $('#studentList').css('border-top', 'none');
-    $('#studentList').css('border-right', 'none');
-    $('th').attr('style', " border-top: 1px solid #dddddd");
-    $('#studentList').find('thead tr th:last-child').css('border', 'none');
-
-    styleEditBtn(".editButton");
-
     initManagement(".editButton", "Редактировать", "Редактирование студент", "#studentList");
 };
 
 function initLecturerManagement() {
-
-    $('#professorsList').css('border-top', 'none');
-    $('#professorsList').css('border-right', 'none');
-    $('th').attr('style', " border-top: 1px solid #dddddd");
-    $('#professorsList').find('thead tr th:last-child').css('border', 'none');
-
-    styleEditBtn(".editButton");
-
-
-    $('#buttonActionSection').insertBefore('#professorsList');
-    styleAddBtn("#buttonActionSection");
-
     initManagement(".addButton", "Добавить преподавателя", "Добавление преподавателя", "#professorsList");
     initManagement(".editButton", "Редактировать", "Редактирование преподавателя", "#professorsList");
 };
 
 function initGroupManagement() {
-    $('#buttonActionSection').insertBefore('#groupList');
-    styleAddBtn("#buttonActionSection");
     initManagement(".addButton", "Добавить группу", "Добавление группы", "#groupList");
-
 };
 
 function initManagement(btnSelector, btnTooltipTitle, saveDialogTitle, updateTableId) {
@@ -58,34 +36,6 @@ function updateTable(updateTableId) {
     if (updateTableId) {
         $(updateTableId).dataTable().fnDraw();
     }
-};
-
-function styleAddBtn(selector) {
-    $(selector).css({
-        'display': 'inline-block',
-        'float': 'left',
-        'margin': '15px 0px 0px 15px',
-        'width': '30px',
-        'height': '30px',
-        'padding': '7px',
-        'border-top-left-radius': '5px',
-    });
-    
-    $(selector).addClass('managementBtn');
-    $(selector).find('.glyphicon').css({ 'color': '#ffffff' });
-};
-
-function styleEditBtn(selector) {
-    var cells = $(selector).parent('td');
-    cells.css({ 'border': 'none' });
-    cells.find('.glyphicon').css({ 'color': '#ffffff' });
-
-    $(cells).addClass('managementBtn');
-    
-    $(cells).css({
-        'width': '35px',
-        'border-top-right-radius': '10px',
-    });
 };
 
 
