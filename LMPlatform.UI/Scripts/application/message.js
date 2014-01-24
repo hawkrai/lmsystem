d1b2b86eeda131f31da280b9de2dc5efb7f6dd46
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-01-24 13:09:35 +0000

    Added chosen.jquery
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@62 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- LMPlatform.UI/Scripts/application/message.js
+++ LMPlatform.UI/Scripts/application/message.js
@@ -1,12 +1,3 @@
-﻿function addRecipient(recipientId, recipientName) {
-    if (recipientId != '')
-        addRecipientHtml($('#msgRecipients'), recipientId, recipientName);
-};
-
-function addRecipientHtml(recipientContainer, recipientId, recipientName) {
-    var index = $(recipientContainer).find(".recipient").length;
-    var recipientHtml = "<span class='recipient'><input type=hidden name='Recipients[" + index + "]' value='" + recipientId
-        + "'/>" + recipientName + "; <sup><a href='#'>[Удалить]</a><sup></span>";
-
-    $(recipientContainer).append(recipientHtml);
-};
+﻿$(function () {
+    $(".chosen-select").chosen({ no_results_text: "Пользователь не найден..." });
+});
