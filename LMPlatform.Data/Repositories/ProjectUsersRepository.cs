b96bdadbbdfc8df4d762430a923babc6d26dab13
infantika <infantika@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-04-03 13:57:26 +0000

    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@98 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.Data/Repositories/ProjectUsersRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Data;
+using LMPlatform.Data.Infrastructure;
+using LMPlatform.Data.Repositories.RepositoryContracts;
+using LMPlatform.Models;
+
+namespace LMPlatform.Data.Repositories
+{
+    public class ProjectUsersRepository : RepositoryBase<LmPlatformModelsContext, ProjectUser>, IProjectUsersRepository
+    {
+        public ProjectUsersRepository(LmPlatformModelsContext dataContext) : base(dataContext)
+        {
+        }
+    }
+}
