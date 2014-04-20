aa295c1a4e7a86e7afd37aae7e1c2059a26fde9d
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-04-20 19:52:50 +0000

    added files
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@114 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/ViewModels/ParentalViewModels/ParentalViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Application.Core;
+using Application.Infrastructure.StudentManagement;
+using Application.Infrastructure.SubjectManagement;
+using LMPlatform.Models;
+
+namespace LMPlatform.UI.ViewModels.ParentalViewModels
+{
+    public class ParentalViewModel
+    {
+        private readonly LazyDependency<IStudentManagementService> _studentManagementService = new LazyDependency<IStudentManagementService>();
+        private readonly LazyDependency<ISubjectManagementService> _subjectManagementService = new LazyDependency<ISubjectManagementService>();
+
+        public IStudentManagementService StudentManagementService
+        {
+            get
+            {
+                return _studentManagementService.Value;
+            }
+        }
+
+        public ISubjectManagementService SubjectManagementService
+        {
+            get
+            {
+                return _subjectManagementService.Value;
+            }
+        }
+
+        public ParentalViewModel()
+        {
+        }
+
+        public ParentalViewModel(Group group)
+        {
+            Group = group;
+            Subjects = SubjectManagementService.GetGroupSubjects(group.Id);
+            Students = StudentManagementService.GetGroupStudents(group.Id).ToList();
+        }
+
+        public Group Group { get; set; }
+
+        public List<Student> Students { get; set; }
+
+        public List<Subject> Subjects { get; set; }
+    }
+}
---
+++ LMPlatform.UI/ViewModels/ParentalViewModels/StatisticsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LMPlatform.Models;
+
+namespace LMPlatform.UI.ViewModels.ParentalViewModels
+{
+    public class StatisticsViewModel : ParentalViewModel
+    {
+        public StatisticsViewModel(Group group)
+            : base(group)
+        {
+        }
+
+        public IEnumerable<SelectListItem> GetStudentsSelectList()
+        {
+            var items = Students.Select(s => new SelectListItem()
+                        {
+                            Text = s.FullName,
+                            Value = s.Id.ToString(),
+                            Selected = false,
+                        }).OrderBy(e => e.Text).ToList();
+
+            var allItem = new SelectListItem()
+                {
+                    Text = "Все",
+                    Value = "-1",
+                    Selected = true
+                };
+
+            items.Insert(0, allItem);
+
+            return items;
+        }
+    }
+}
