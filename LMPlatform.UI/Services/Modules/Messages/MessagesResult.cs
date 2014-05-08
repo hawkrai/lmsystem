340a8e6819690d6196aec1299b106d5b4de0e5fd
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-05-08 17:22:02 +0000

    Fixed missing data
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@144 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/Services/Modules/Messages/MessagesResult.cs
@@ -0,0 +1,12 @@
+﻿namespace LMPlatform.UI.Services.Modules.Messages
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class MessagesResult : ResultViewData
+    {
+        [DataMember]
+        public List<MessagesViewData> Messages { get; set; } 
+    }
+}
---
+++ LMPlatform.UI/Services/Modules/Messages/MessagesViewData.cs
@@ -0,0 +1,55 @@
+﻿namespace LMPlatform.UI.Services.Modules.Messages
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using System.ServiceModel.Description;
+
+    using Application.Core;
+    using Application.Infrastructure.FilesManagement;
+    using Application.Infrastructure.MessageManagement;
+
+    using LMPlatform.Models;
+
+    [DataContract]
+    public class MessagesViewData
+    {
+        private readonly LazyDependency<IMessageManagementService> messageManagementService = new LazyDependency<IMessageManagementService>();
+        private readonly LazyDependency<IFilesManagementService> filesManagementService = new LazyDependency<IFilesManagementService>();
+
+        public IFilesManagementService FilesManagementService
+        {
+            get
+            {
+                return filesManagementService.Value;
+            }
+        }
+
+        public IMessageManagementService MessageManagementService
+        {
+            get
+            {
+                return messageManagementService.Value;
+            }
+        }
+
+        public MessagesViewData(UserMessages userMessage)
+        {
+            AthorName = userMessage.Author.FullName;
+            Subject = userMessage.Message.Subject;
+            Body = userMessage.Message.Text;
+            IsRead = userMessage.IsRead;
+        }
+
+        [DataMember]
+        public string AthorName { get; set; }
+
+        [DataMember]
+        public string Subject { get; set; }
+
+        [DataMember]
+        public string Body { get; set; }
+
+        [DataMember]
+        public bool IsRead { get; set; }
+    }
+}
