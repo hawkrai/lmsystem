cd03c3e3c1585a28d513a83176d8dabadb7c0be1
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-05-20 15:25:47 +0000

    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@169 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- LMPlatform.UI/Scripts/application/parentalModule.js
+++ LMPlatform.UI/Scripts/application/parentalModule.js
@@ -2,36 +2,55 @@
 
 parentalApp.controller("StatCtrl", ['$scope', '$http', '$modal', function ($scope, $http, $modal) {
 
+    $scope.data = [];
+
     $scope.UrlServiceParental = '/Services/Parental/ParentalService.svc/';
     $scope.UrlServiceCore = '/Services/CoreService.svc/';
 
     $scope.Subjects = [];
-    $scope.subject = { Name: "Все предметы", Id: -1, ShortName: "" };
+    $scope.subject = { Name: "Все предметы", Id: 0, ShortName: "" };
     $scope.studentsStat = [];
 
     $scope.init = function (groupId) {
+        $scope.data = [];
+        $scope.data.Students = {};
         $scope.groupId = groupId;
         $scope.loadSubjects();
     };
 
     $scope.$watch("subject", function (newValue, oldValue) {
-        if (newValue.Id < 0) {
-            $scope.data = {};
-            $scope.data.Students = {};
+        if (newValue.Id <= 0) {
+
         } else {
-            $scope.loadData($scope.subject.Id);
+            if ($scope.data.length > 0)
+                $scope.initStudentsStat(newValue.Id);
         }
     });
 
     $scope.$watch("data", function (newValue, oldValue) {
+        $scope.initStudentsStat();
+    });
+
+    $scope.initStudentsStat = function (subjectId) {
         $scope.studentsStat = [];
-        if (newValue.Students.length) {
-            newValue.Students.forEach(
+        var data = $scope.data[subjectId];
+
+        if (data.SubGroupsOne.Students.length) {
+
+            data.SubGroupsOne.Students.forEach(
                 function (element, index) {
-                    $scope.studentsStat[index] = getStudentStat(element);
+                    $scope.studentsStat[index] = getStudentStat(element, data);
                 });
         }
-    });
+
+        if (data.SubGroupsTwo.Students.length) {
+            var length = $scope.studentsStat.length;
+            data.SubGroupsTwo.Students.forEach(
+                function (element, index) {
+                    $scope.studentsStat[length + index] = getStudentStat(element, data);
+                });
+        }
+    };
 
 
     $scope.loadSubjects = function () {
@@ -45,8 +64,16 @@ parentalApp.controller("StatCtrl", ['$scope', '$http', '$modal', function ($scop
             } else {
 
                 if (data.Subjects.length > 0) {
+
+
                     $scope.Subjects = data.Subjects;
                     $scope.Subjects.splice(0, 0, $scope.subject);
+
+                    data.Subjects.forEach(function (val) {
+                        if (val.Id) {
+                            $scope.loadData(val.Id);
+                        }
+                    });
                 }
 
                 alertify.success(data.Message);
@@ -65,32 +92,77 @@ parentalApp.controller("StatCtrl", ['$scope', '$http', '$modal', function ($scop
             } else {
 
                 var queryData = Enumerable.From(data.Groups).First(function (x) { return x.GroupId == $scope.groupId; });
-
-                $scope.data = queryData;
-
+                $scope.data[subjectId] = queryData;
 
                 alertify.success(data.Message);
             }
         });
     };
 
-    var getStudentStat = function (student) {
+    var getStudentStat = function (subGroupStudent, data) {
         var studentStat = {
-            Name: student.FullName,
-            LecHours: 0
+            Name: subGroupStudent.FullName,
+            LecHours: 0,
+            PractHours: 0,
+            LabHours: 0,
+            TotalHours: 0,
+            LabMark: 0,
+            PractMark: 0,
+            LabsCount: 0,
+            PractsCount: 0
         };
 
-        var lecMarks = Enumerable.From($scope.data.LecturesMarkVisiting).First(function (x) { return x.StudentId == student.StudentId; }).Marks;
+        var studentObj = Enumerable.From(data.Students).First(function (x) { return x.StudentId == subGroupStudent.StudentId; });
 
-        studentStat.LecHours = 0;
+        var lecVisit = Enumerable.From(data.LecturesMarkVisiting).First(function (x) { return x.StudentId == subGroupStudent.StudentId; }).Marks;
+        var lecVisitArray = Enumerable.From(lecVisit).Select(function (x) { return x.Mark; }).ToArray();
 
-        lecMarks.forEach(function (mark) {
-            var hours = parseInt(mark.Mark);
-            if (hours)
-                studentStat.LecHours += hours;
-        });
+        var pracVisitArray = Enumerable.From(studentObj.PracticalVisitingMark).Select(function (x) { return x.Mark; }).ToArray();
+        var pracMarkArray = Enumerable.From(studentObj.StudentPracticalMarks).Select(function (x) { return x.Mark; }).ToArray();
+
+        var labVisitArray = Enumerable.From(subGroupStudent.LabVisitingMark).Select(function (x) { return x.Mark; }).ToArray();
+        var labMarkArray = Enumerable.From(subGroupStudent.StudentLabMarks).Select(function (x) { return x.Mark; }).ToArray();
+
+        var lecVisitResult = markArrayProc(lecVisitArray);
+        var practVisitResult = markArrayProc(pracVisitArray);
+        var labVisitResult = markArrayProc(labVisitArray);
+
+        studentStat.LecHours = lecVisitResult.sum <= 0 ? "-" : lecVisitResult.sum;
+        studentStat.PractHours = practVisitResult.sum <= 0 ? "-" : practVisitResult.sum;
+        studentStat.LabHours = labVisitResult.sum <= 0 ? "-" : labVisitResult.sum;
+        studentStat.TotalHours = lecVisitResult.sum + practVisitResult.sum + labVisitResult.sum;
+
+        var labMarkResult = markArrayProc(labMarkArray);
+        var practMarkResult = markArrayProc(pracMarkArray);
+
+        studentStat.LabMark = labMarkResult.avg;
+        studentStat.PractMark = practMarkResult.avg;
+        studentStat.LabsCount = labMarkResult.pos;
+        studentStat.PractsCount = practMarkResult.pos;
 
         return studentStat;
+    };
+
+    var markArrayProc = function (arr) {
+        var result = { sum: 0, avg: 0, pos: 0, neg: 0 };
+
+        if (arr && arr.length > 0) {
+            arr.forEach(function (val) {
+                var value = parseInt(val);
+
+                if (value && value > 0) {
+                    result.sum += value;
+                } else {
+                    result.neg += 1;
+                }
+            });
+
+            result.pos = arr.length - result.neg;
+            if (result.pos > 0)
+                result.avg = result.sum / result.pos;
+        }
+
+        return result;
     };
 
 }]);
