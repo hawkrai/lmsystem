af317c54d663a13e54bf8976a03dde2a67cd3d94
kiryl22 <kiryl22@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-06-10 00:46:22 +0000

    fixed missing files
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@211 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/Services/Files/FilesService.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LMPlatform.UI.Services.Files
+{
+    using System.Configuration;
+
+    using Application.Core;
+    using Application.Infrastructure.FilesManagement;
+    using Application.Infrastructure.MessageManagement;
+
+    using LMPlatform.UI.Services.Modules.Lectures;
+    using LMPlatform.UI.Services.Modules.Messages;
+
+    public class FilesService : IFilesService
+    {
+        private readonly LazyDependency<IFilesManagementService> _filesManagementService =
+                                                new LazyDependency<IFilesManagementService>();
+
+        public IFilesManagementService FilesManagementService
+        {
+            get { return _filesManagementService.Value; }
+        }
+
+        public AttachmentResult GetFiles()
+        {
+            try
+            {
+                var attachments = FilesManagementService.GetAttachments(null).ToList();
+                string storageRoot = ConfigurationManager.AppSettings["FileUploadPath"];
+                var result = new AttachmentResult
+                {
+                    Files = attachments,
+                    ServerPath = storageRoot,
+                    Message = "Данные успешно загружены",
+                    Code = "200"
+                };
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new AttachmentResult
+                {
+                    Message = "Произошла ошибка при получении данных",
+                    Code = "500"
+                };
+            }
+        }
+    }
+}
---
+++ LMPlatform.UI/Services/Modules/Files/AttachmentResult.cs
@@ -0,0 +1,17 @@
+﻿namespace LMPlatform.UI.Services.Modules.Messages
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    using LMPlatform.Models;
+
+    [DataContract]
+    public class AttachmentResult : ResultViewData
+    {
+        [DataMember]
+        public List<Attachment> Files { get; set; }
+
+        [DataMember]
+        public string ServerPath { get; set; }
+    }
+}
