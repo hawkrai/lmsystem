42147008cdf50efdbd845a1e18829649656113b6
infantika <infantika@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-06-17 14:00:32 +0000

    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@246 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.Models/BugLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Data;
+
+namespace LMPlatform.Models
+{
+    public class BugLog : ModelBase
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime LogDate { get; set; }
+
+        public int BugId { get; set; }
+
+        public int PrevStatusId { get; set; }
+
+        public int CurrStatusId { get; set; }
+
+        public Bug Bug
+        {
+            get;
+            set;
+        }
+
+        public User User
+        {
+            get;
+            set;
+        }
+    }
+}
