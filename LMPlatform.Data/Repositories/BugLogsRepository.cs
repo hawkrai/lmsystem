b388ebecac746b5a1ad6e994859c9126071ff952
infantika <infantika@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-06-17 09:47:26 +0000

    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@243 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.Data/Repositories/BugLogsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Data;
+using LMPlatform.Data.Infrastructure;
+using LMPlatform.Data.Repositories.RepositoryContracts;
+using LMPlatform.Models;
+
+namespace LMPlatform.Data.Repositories
+{
+    public class BugLogsRepository : RepositoryBase<LmPlatformModelsContext, BugLog>, IBugLogsRepository
+    {
+        public BugLogsRepository(LmPlatformModelsContext dataContext) : base(dataContext)
+        {
+        }
+
+        public void DeleteBugLog(BugLog bugLog)
+        {
+            using (var context = new LmPlatformModelsContext())
+            {
+                var model = context.Set<BugLog>().FirstOrDefault(e => e.Id == bugLog.Id);
+                context.Delete(model);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ LMPlatform.Data/Repositories/RepositoryContracts/IBugLogsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Data;
+using LMPlatform.Models;
+
+namespace LMPlatform.Data.Repositories.RepositoryContracts
+{
+    public interface IBugLogsRepository : IRepositoryBase<BugLog>
+    {
+        void DeleteBugLog(BugLog bugLog);
+    }
+}
