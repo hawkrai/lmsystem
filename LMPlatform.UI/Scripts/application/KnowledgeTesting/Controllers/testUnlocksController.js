12ffb2a56327bf1afcb5a7bf0b79772f4061c3d0
girl10011 <girl10011@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-06-17 07:34:42 +0000

    test unlocks
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@242 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- LMPlatform.UI/Scripts/application/KnowledgeTesting/Controllers/testUnlocksController.js
+++ LMPlatform.UI/Scripts/application/KnowledgeTesting/Controllers/testUnlocksController.js
@@ -9,9 +9,11 @@ knowledgeTestingApp.controller('testUnlocksCtrl', function ($scope, $http, id, $
     $http({ method: "GET", url: kt.actions.groups.getGroupsForSubject, dataType: 'json', params: { subjectId: subjectId } })
             .success(function (data) {
                 $scope.groups = data;
-                //$scope.selectedGroup = data[0];
+            if (data.length > 0) {
+                $scope.group = data[0];
                 $scope.loadStudents();
-            })
+            }
+        })
             .error(function (data, status, headers, config) {
                 alertify.error("Во время получения данных о группах произошла ошибка");
             });
@@ -26,6 +28,45 @@ knowledgeTestingApp.controller('testUnlocksCtrl', function ($scope, $http, id, $
                     alertify.error("Во время получения данных о подгруппах произошла ошибка");
                 });
         }
+    };
+
+    $scope.lockAll = function (subGroup, unlock) {
+        var studentIds = Enumerable.From(subGroup.Students)
+           .Select(function (student) {
+               return student.Id;
+           }).ToArray();
+
+        var model = {
+            studentIds: studentIds,
+            testId: id,
+            unlock: unlock
+        };
+
+        $.ajax({
+            url: kt.actions.tests.unlock,
+            type: "POST",
+            data: JSON.stringify(model),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function () { $scope.loadStudents(); }
+        });
+    };
+
+    $scope.lockOne = function(studentId, unlocked) {
+        var model = {
+            testId: id,
+            studentId: studentId,
+            unlocked: unlocked
+        };
+
+        $.ajax({
+            url: kt.actions.tests.unlockOne,
+            type: "POST",
+            data: JSON.stringify(model),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function () { $scope.loadStudents(); }
+        });
     };
     
     $scope.closeDialog = function () {
