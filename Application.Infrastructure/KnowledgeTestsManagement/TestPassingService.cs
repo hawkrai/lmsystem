5d589eab1dac12b1b92cb8bd4b6836545389c0d0
girl10011 <girl10011@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-09-09 06:08:55 +0000

    Throw exception if test has no questions
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@273 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- Application.Infrastructure/KnowledgeTestsManagement/TestPassingService.cs
+++ Application.Infrastructure/KnowledgeTestsManagement/TestPassingService.cs
@@ -566,6 +566,12 @@ namespace Application.Infrastructure.KnowledgeTestsManagement
         private void StartNewTest(int testId, int userId)
         {
             Test test = GetTest(testId);
+
+            if (test.Questions.Count == 0)
+            {
+                throw new InvalidDataException("В тесте не содержится ни одного вопроса");
+            }
+            
             int questionsCount = test.CountOfQuestions > test.Questions.Count
                 ? test.Questions.Count
                 : test.CountOfQuestions;
