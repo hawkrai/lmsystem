10a9fc99afbf98dbe99df8b195b4c5796e0e9dce
hawkrai <hawkrai@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-09-15 07:51:18 +0000

    add zip file utils
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@274 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ Application.Infrastructure/UtilZip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zip;
+using LMPlatform.Models;
+
+namespace Application.Infrastructure
+{
+	public static class UtilZip
+	{
+		public static ZipFile CreateZipFile(string path, ZipFile zip, List<Attachment> files, string userName)
+		{
+			//var zip = new ZipFile(Encoding.UTF8);
+			zip.AddDirectoryByName(userName);
+			foreach (var fileName in files)
+			{
+				if (File.Exists(path + fileName.PathName + "//" + fileName.FileName))
+				{
+					zip.AddFile(path + fileName.PathName + "//" + fileName.FileName, userName).FileName = "\\" + userName  + "\\" + fileName.Name;
+				}
+			}
+
+			return zip;
+		}
+	}
+}
