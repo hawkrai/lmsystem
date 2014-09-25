7a885ea4701cd6567f2f70e96b7edd49c918f755
girl10011 <girl10011@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-09-25 12:10:29 +0000

    Automatio update on control page
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@294 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- LMPlatform.UI/Scripts/application/KnowledgeTesting/Controllers/controlController.js
+++ LMPlatform.UI/Scripts/application/KnowledgeTesting/Controllers/controlController.js
@@ -12,17 +12,17 @@ knowledgeTestingApp.controller('controlCtrl', function ($scope, $http, $modal) {
             alertify.error("Во время получения данных произошла ошибка");
         });
 
-    //$scope.repeat = function() {
-    //    window.setInterval(function() {
+    $scope.repeat = function() {
+        window.setInterval(function() {
 
-    //        $http({ method: "GET", url: kt.actions.results.getControl, dataType: 'json', params: { subjectId: $scope.subjectId } })
-    //            .success(function(data) {
-    //                $scope.items = data;
-    //                $scope.repeat();
-    //            })
-    //            .error(function(data, status, headers, config) {
-    //                alertify.error("Во время получения данных произошла ошибка");
-    //            });
-    //    }, 1000);
-    //};
+            $http({ method: "GET", url: kt.actions.results.getControl, dataType: 'json', params: { subjectId: $scope.subjectId } })
+                .success(function(data) {
+                    $scope.items = data;
+                    $scope.repeat();
+                })
+                .error(function(data, status, headers, config) {
+                    alertify.error("Во время получения данных произошла ошибка");
+                });
+        }, 1000);
+    };
 });
