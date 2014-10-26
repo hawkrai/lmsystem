09c1094528a074276a7e416186e4854757b5c92f
hawkrai <hawkrai@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2014-10-26 17:37:47 +0000

    fix last commit
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@300 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.UI/Templates/js/AdminLTE/app.js
@@ -0,0 +1,195 @@
+/*!
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This file should be included in all pages
+ !**/
+
+/*
+ * Global variables. If you change any of these vars, don't forget 
+ * to change the values in the less files!
+ */
+var left_side_width = 220; //Sidebar width in pixels
+
+$(function() {
+    "use strict";
+
+    //Enable sidebar toggle
+    $("[data-toggle='offcanvas']").click(function(e) {
+        e.preventDefault();
+
+        //If window is small enough, enable sidebar push menu
+        if ($(window).width() <= 992) {
+            $('.row-offcanvas').toggleClass('active');
+            $('.left-side').removeClass("collapse-left");
+            $(".right-side").removeClass("strech");
+            $('.row-offcanvas').toggleClass("relative");
+        } else {
+            //Else, enable content streching
+            $('.left-side').toggleClass("collapse-left");
+            $(".right-side").toggleClass("strech");
+        }
+    });
+
+    //Add hover support for touch devices
+    $('.btn').bind('touchstart', function() {
+        $(this).addClass('hover');
+    }).bind('touchend', function() {
+        $(this).removeClass('hover');
+    });
+
+    //Activate tooltips
+    $("[data-toggle='tooltip']").tooltip();
+
+    /*     
+     * Add collapse and remove events to boxes
+     */
+    $("[data-widget='collapse']").click(function() {
+        //Find the box parent        
+        var box = $(this).parents(".box").first();
+        //Find the body and the footer
+        var bf = box.find(".box-body, .box-footer");
+        if (!box.hasClass("collapsed-box")) {
+            box.addClass("collapsed-box");
+            //Convert minus into plus
+            $(this).children(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+            bf.slideUp();
+        } else {
+            box.removeClass("collapsed-box");
+            //Convert plus into minus
+            $(this).children(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+            bf.slideDown();
+        }
+    });
+
+    /*
+     * ADD SLIMSCROLL TO THE TOP NAV DROPDOWNS
+     * ---------------------------------------
+     */
+    //$(".navbar .menu").slimscroll({
+    //    height: "200px",
+    //    alwaysVisible: false,
+    //    size: "3px"
+    //}).css("width", "100%");
+
+    /*
+     * INITIALIZE BUTTON TOGGLE
+     * ------------------------
+     */
+    $('.btn-group[data-toggle="btn-toggle"]').each(function() {
+        var group = $(this);
+        $(this).find(".btn").click(function(e) {
+            group.find(".btn.active").removeClass("active");
+            $(this).addClass("active");
+            e.preventDefault();
+        });
+
+    });
+
+    $("[data-widget='remove']").click(function() {
+        //Find the box parent        
+        var box = $(this).parents(".box").first();
+        box.slideUp();
+    });
+
+    /* Sidebar tree view */
+    $(".sidebar .treeview").tree();
+
+    /* 
+     * Make sure that the sidebar is streched full height
+     * ---------------------------------------------
+     * We are gonna assign a min-height value every time the
+     * wrapper gets resized and upon page load. We will use
+     * Ben Alman's method for detecting the resize event.
+     * 
+     **/
+    function _fix() {
+        //Get window height and the wrapper height
+        var height = $(window).height() - $("body > .header").height() - ($("body > .footer").outerHeight() || 0);
+        $(".wrapper").css("min-height", height + "px");
+        var content = $(".wrapper").height();
+        //If the wrapper height is greater than the window
+        if (content > height)
+            //then set sidebar height to the wrapper
+            $(".left-side, html, body").css("min-height", content + "px");
+        else {
+            //Otherwise, set the sidebar to the height of the window
+            $(".left-side, html, body").css("min-height", height + "px");
+        }
+    }
+    //Fire upon load
+    _fix();
+    //Fire when wrapper is resized
+    $(".wrapper").resize(function() {
+        _fix();
+        fix_sidebar();
+    });
+
+    //Fix the fixed layout sidebar scroll bug
+    fix_sidebar();
+
+    /*
+     * We are gonna initialize all checkbox and radio inputs to 
+     * iCheck plugin in.
+     * You can find the documentation at http://fronteed.com/iCheck/
+     */
+    $("input[type='checkbox']:not(.simple), input[type='radio']:not(.simple)").iCheck({
+        checkboxClass: 'icheckbox_minimal',
+        radioClass: 'iradio_minimal'
+    });
+
+});
+
+function fix_sidebar() {
+	//Make sure the body tag has the .fixed class
+	if (!$("body").hasClass("fixed")) {
+		return;
+	}
+}
+
+$(function($) {
+		"use strict";
+
+		$.fn.tree = function() {
+
+			return this.each(function() {
+				var btn = $(this).children("a").first();
+				var menu = $(this).children(".treeview-menu").first();
+				var isActive = $(this).hasClass('active');
+
+				//initialize already active menus
+				if (isActive) {
+					menu.show();
+					btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+				}
+				//Slide open or close the menu on link click
+				btn.click(function(e) {
+					e.preventDefault();
+					if (isActive) {
+						//Slide up to close menu
+						menu.slideUp();
+						isActive = false;
+						btn.children(".fa-angle-down").first().removeClass("fa-angle-down").addClass("fa-angle-left");
+						btn.parent("li").removeClass("active");
+					} else {
+						//Slide down to open menu
+						menu.slideDown();
+						isActive = true;
+						btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+						btn.parent("li").addClass("active");
+					}
+				});
+
+				/* Add margins to submenu elements to give it a tree look */
+				menu.find("li > a").each(function() {
+					var pad = parseInt($(this).css("margin-left")) + 10;
+
+					$(this).css({ "margin-left": pad + "px" });
+				});
+
+			});
+
+		};
+
+
+	}(jQuery));
