2c749a50f038f094621c83b4ecb500516ac4c189
hawkrai <hawkrai@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2015-01-30 07:33:13 +0000

    fix bugs
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@318 5540d691-cd67-41f9-94aa-ef72a3cc35fd

--- LMPlatform.UI/Scripts/application/mainApp.js
+++ LMPlatform.UI/Scripts/application/mainApp.js
@@ -45,46 +45,44 @@ angular.module('mainApp.controllers', ['ui.bootstrap', 'xeditable', 'textAngular
         $scope.renderHtml = function (htmlCode) {
             return $sce.trustAsHtml(htmlCode);
         };
-        $scope.today = function () {
-            $scope.dt = new Date();
+        
+        //$scope.today = function () {
+        //	$scope.dt = new Date();
+        //};
+        //$scope.today();
+
+        $scope.clear = function () {
+        	$scope.dt = null;
         };
-        $scope.today();
+
+    	// Disable weekend selection
+        $scope.disabled = function (date, mode) {
+        	return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+        };
+
+        $scope.toggleMin = function () {
+        	$scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function () {
+        	//$event.preventDefault();
+        	//$event.stopPropagation();
+
+        	$scope.opened = true;
+        };
+        
+        $scope.dateOptions = {
+        	formatYear: 'yyyy',
+        	startingDay: 1
+        };
+
+        $scope.format = "dd/MM/yyyy";
 
         $scope.showWeeks = true;
         $scope.toggleWeeks = function () {
             $scope.showWeeks = !$scope.showWeeks;
         };
-
-        $scope.clear = function () {
-            $scope.dt = null;
-        };
-
-        // Disable weekend selection
-        $scope.disabled = function (date, mode) {
-            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
-        };
-
-        $scope.toggleMin = function () {
-            $scope.minDate = ($scope.minDate) ? null : new Date();
-        };
-        $scope.toggleMin();
-
-        $scope.openDate = function ($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.opened = true;
-        };
-
-        $scope.dateOptions = {
-            'year-format': "'yy'",
-            'starting-day': 1,
-
-        };
-
-        $scope.formats = ['dd/MM/yyyy', 'yyyy/MM/dd', 'shortDate'];
-        $scope.format = $scope.formats[0];
-
 
         $scope.subjectId = 0;
         $scope.UrlServiceMain = '/Services/CoreService.svc/';
