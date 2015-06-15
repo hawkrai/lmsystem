8fc210433ef3decb3eed3fdec12990dbe8e6ff25
hawkrai <hawkrai@5540d691-cd67-41f9-94aa-ef72a3cc35fd>
2015-06-15 05:04:33 +0000

    fix commit
    git-svn-id: https://svn.code.sf.net/p/lmsystem/code@360 5540d691-cd67-41f9-94aa-ef72a3cc35fd

---
+++ LMPlatform.Models/ScoObjects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Data;
+
+namespace LMPlatform.Models
+{
+	public class ScoObjects : ModelBase
+	{
+		public string Name { get; set; }
+
+		public string Path { get; set; }
+	}
+}
--- LMPlatform.UI/Scripts/application/scorm/scorm-controller.js
+++ LMPlatform.UI/Scripts/application/scorm/scorm-controller.js
@@ -1,87 +1,89 @@
 ﻿angular.module('scormApp.controllers', [])
     .controller('ScormController', function ($scope, $http) {
 
-        $scope.sco = [];
-        $scope.viewScoClient = false;
-        $scope.urlServiceCore = "";
-        $scope.treeActivity = null;
-        $scope.nameLoadSco = "";
-        $scope.viewScoObject = null;
+    	$scope.sco = [];
+    	$scope.viewScoClient = false;
+    	$scope.urlServiceCore = "";
+    	$scope.treeActivity = null;
+    	$scope.nameLoadSco = "";
+    	$scope.viewScoObject = null;
 
-        $scope.init = function () {
-            $scope.loadObjects();
-        };
+    	$scope.init = function () {
+    		$scope.loadObjects();
+    	};
 
-        $scope.closeSco = function() {
-            $scope.viewScoClient = false;
-            $scope.viewScoObject = null;
-            $scope.urlServiceCore = "";
-        };
+    	$scope.closeSco = function () {
+    		$scope.viewScoClient = false;
+    		$scope.viewScoObject = null;
+    		$scope.urlServiceCore = "";
+    	};
 
-        $scope.frameLoad = function(urlRes) {
-            $scope.urlServiceCore = "/ScormObjects/" + $scope.viewScoObject.Path + "/" + urlRes;
-        };
+    	$scope.frameLoad = function (urlRes) {
+    		$scope.urlServiceCore = "/ScormObjects/" + $scope.viewScoObject.Path + "/" + urlRes;
+    	};
 
-        $scope.viewSco = function (object) {
-            $scope.viewScoClient = true;
-            $scope.viewScoObject = object;
-            
-            $.ajax({
-                type: 'GET',
-                url: "/ScormMod/ViewSco?path=" + object.Path,
-                dataType: "json",
-                contentType: "application/json",
+    	$scope.viewSco = function (object) {
+    		$scope.viewScoClient = true;
+    		$scope.viewScoObject = object;
 
-            }).success(function (data, status) {
-                $scope.$apply(function () {
-                    $scope.treeActivity = data;
-                });
-               
-            });
-        };
+    		$.ajax({
+    			type: 'GET',
+    			url: "/ScormMod/ViewSco?path=" + object.Path,
+    			dataType: "json",
+    			contentType: "application/json",
 
-        $scope.loadObjects = function() {
-            $.ajax({
-                type: 'GET',
-                url: "/ScormMod/GetObjects",
-                dataType: "json",
-                contentType: "application/json",
-
-            }).success(function (data, status) {
-                $scope.$apply(function () {
-                        $scope.sco = data;
-                    });
-
-            });
-        };
-        
-        $scope.openSco = function () {
-            $($.find('input[type=file][name=openSco]')[0]).trigger('click');
-        };
-
-        $scope.loadScoEventClick = function () {
-            var formData = new FormData();
+    		}).success(function (data, status) {
+    			$scope.$apply(function () {
+    				$scope.treeActivity = data;
+    			});
 
 
-            formData.append("name", $scope.nameLoadSco);
-            formData.append("file", $.find('input[type=file][name=openSco]')[0].files[0]);
-            $.ajax({
-                url: "/ScormMod/LoadObject",
-                type: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: function (data) {
-                    $scope.$apply(function () {
-                        $scope.loadObjects();
-                    });
-                    
-                },
-            });
-            $('#dialogLoadSco').modal('hide');
-        };
 
-        $scope.loadSco = function () {
-            $('#dialogLoadSco').modal();
-        };
+    		});
+    	};
+
+    	$scope.loadObjects = function () {
+    		$.ajax({
+    			type: 'GET',
+    			url: "/ScormMod/GetObjects",
+    			dataType: "json",
+    			contentType: "application/json",
+
+    		}).success(function (data, status) {
+    			$scope.$apply(function () {
+    				$scope.sco = data;
+    			});
+
+    		});
+    	};
+
+    	$scope.openSco = function () {
+    		$($.find('input[type=file][name=openSco]')[0]).trigger('click');
+    	};
+
+    	$scope.loadScoEventClick = function () {
+    		var formData = new FormData();
+
+
+    		formData.append("name", $scope.nameLoadSco);
+    		formData.append("file", $.find('input[type=file][name=openSco]')[0].files[0]);
+    		$.ajax({
+    			url: "/ScormMod/LoadObject",
+    			type: 'POST',
+    			data: formData,
+    			processData: false,
+    			contentType: false,
+    			success: function (data) {
+    				$scope.$apply(function () {
+    					$scope.loadObjects();
+    				});
+
+    			},
+    		});
+    		$('#dialogLoadSco').modal('hide');
+    	};
+
+    	$scope.loadSco = function () {
+    		$('#dialogLoadSco').modal();
+    	};
     });
