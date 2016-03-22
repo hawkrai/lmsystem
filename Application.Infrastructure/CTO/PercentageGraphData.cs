3a6ee4a0899eb1335f2caf17634602f0a1a3dccf
ErmakEvg <dvarfik1ermak@mail.ru>
2016-03-22 14:20:48 +0300

    add cto from course projects

---
+++ Application.Infrastructure/CTO/PercentageGraphData.cs
@@ -0,0 +1,21 @@
+﻿using LMPlatform.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Application.Infrastructure.CTO
+{
+    public class PercentageGraphData
+    {
+        public int? Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public double Percentage { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int SubjectId { get; set; }
+
+        public IEnumerable<int> SelectedGroupsIds { get; set; }
+    }
+}
---
+++ Application.Infrastructure/CTO/PercentageResultData.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Infrastructure.CTO
+{
+    public class PercentageResultData
+    {
+        public int? Id { get; set; }
+
+        public int PercentageGraphId { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int? Mark { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
