7c87b342dff465c8e4dbd98527edb86274dbee83
GogolushkoMike <mikhail.gogolushko@walletone.com>
2016-04-05 14:29:14 +0300

    SearchEngine: Add search result page

---
+++ LMPlatform.UI/ViewModels/SearchViewModel/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using LMPlatform.Models;
+
+namespace LMPlatform.UI.ViewModels.SearchViewModel
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Group> Groups { get; set; }
+        public IEnumerable<Project> Projects { get; set; }
+        public IEnumerable<Lecturer> Lecturers { get; set; }
+    }
+}
