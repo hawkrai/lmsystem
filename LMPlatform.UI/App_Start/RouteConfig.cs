de61c6200ac13af9bdd3c0782fdefe8f68fa1737
Egor Rybchenko <macmaczhl@gmail.com>
2017-04-04 22:05:36 +0300

    Add Bugs page

--- LMPlatform.UI/App_Start/RouteConfig.cs
+++ LMPlatform.UI/App_Start/RouteConfig.cs
@@ -29,6 +29,11 @@ namespace LMPlatform.UI
                 defaults: new { controller = "Profile", action = "Page" });
 
             routes.MapRoute(
+                name: "BTS",
+                url: "BTS/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
---
+++ LMPlatform.UI/Controllers/BTS/BugsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMPlatform.UI.Controllers.BTS
+{
+    public class BugsController : Controller
+    {
+        //
+        // GET: /BTS/Bugs/
+
+        public ActionResult Index()
+        {
+            return View("~/Views/BTS/Bugs/Index.cshtml");
+        }
+    }
+}
