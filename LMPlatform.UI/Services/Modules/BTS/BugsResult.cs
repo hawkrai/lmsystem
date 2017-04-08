38a2ba9c1b3bfdd6399bc1e9e506599a1252d299
Egor Rybchenko <macmaczhl@gmail.com>
2017-04-08 20:58:09 +0300

    Rename

--- LMPlatform.UI/Services/Modules/BTS/BugsResult.cs
+++ LMPlatform.UI/Services/Modules/BTS/BugsResult.cs
@@ -10,7 +10,7 @@ namespace LMPlatform.UI.Services.Modules.BTS
     public class BugsResult : ResultViewData
     {
         [DataMember]
-        public List<BugsViewData> Bugs { get; set; }
+        public List<BugViewData> Bugs { get; set; }
 
         [DataMember]
         public int TotalCount { get; set; }
