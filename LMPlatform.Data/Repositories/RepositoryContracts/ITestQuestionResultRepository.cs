f70ae57361c10c7f339bc388ad7dbe60c8ae3eb0
Sieg228 <sergey.levshunov@gmail.com>
2017-11-30 14:58:45 +0300

    fix

---
+++ LMPlatform.Data/Repositories/RepositoryContracts/ITestQuestionResultRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Core.Data;
+using LMPlatform.Models.KnowledgeTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMPlatform.Data.Repositories.RepositoryContracts
+{
+    public interface ITestQuestionPassResultsRepository : IRepositoryBase<TestQuestionPassResults>
+    {
+        void Create(TestQuestionPassResults item);
+    }
+}
