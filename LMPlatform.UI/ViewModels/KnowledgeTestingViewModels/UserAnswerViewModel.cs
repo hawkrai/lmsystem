b0ebe8563d49e49ddd1d1741259c8f4b1c548046
s.leushunou <s.leushunou@pixelplex.by>
2018-04-17 18:41:49 +0300

    add user test answers result alert

---
+++ LMPlatform.UI/ViewModels/KnowledgeTestingViewModels/UserAnswerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMPlatform.UI.ViewModels.KnowledgeTestingViewModels
+{
+    public class UserAnswerViewModel
+    {
+        public string QuestionTitle { get; set; }
+
+        public string QuestionDescription { get; set; }
+
+        public int Points { get; set; }
+    }
+}
