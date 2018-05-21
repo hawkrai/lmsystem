d4575b2da110545853fa05aa109712bc8fbf5292
s.leushunou <s.leushunou@pixelplex.by>
2018-05-21 17:11:41 +0300

    fix forSelfStudy checkbox

--- Application.Infrastructure/KnowledgeTestsManagement/QuestionsManagementService.cs
+++ Application.Infrastructure/KnowledgeTestsManagement/QuestionsManagementService.cs
@@ -29,6 +29,8 @@ namespace Application.Infrastructure.KnowledgeTestsManagement
             using (var repositoriesContainer = new LmPlatformRepositoriesContainer())
             {
                 if (!repositoriesContainer.TestsRepository.GetBy(testsQuery).ForSelfStudy &&
+                    !repositoriesContainer.TestsRepository.GetBy(testsQuery).ForEUMK &&
+                    !repositoriesContainer.TestsRepository.GetBy(testsQuery).BeforeEUMK &&
                     (repositoriesContainer.TestsRepository.GetBy(testsQuery).TestUnlocks.Count > 0 ||
                     repositoriesContainer.RepositoryFor<AnswerOnTestQuestion>().GetAll(answersQuery).Count() != 0))
                 {
