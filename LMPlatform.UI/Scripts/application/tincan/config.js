933370d98b97ea239ab2329f52e93207339a0680
Veronica Ignatyuk <veronica.ignatyuk@gmail.com>
2018-11-20 23:34:32 +0300

    add UI changes

---
+++ LMPlatform.UI/Scripts/application/tincan/config.js
@@ -0,0 +1,8 @@
+function Config() {
+	"use strict";
+}
+Config.endpoint = "https://desktop-hg0lmqm/data/xAPI/";//TODO: Change url
+Config.user = "username@learninglocker.net";
+Config.password = "Nika123";
+Config.actor = { "name": "username@learninglocker.net", "mbox": "mailto:hello@learninglocker.net", "objectType": "Agent" };
+Config.secret = "Mzc4MzdjMzNhZGQwM2QzNzZiM2Y2NTU2MWYyODU2MzY2NDNmZjg1OTozYzMyNjYxZDVmMjRlOWZmZTJmMDllM2M1ZTc0N2VmNmY2ZmNlOGY3";
