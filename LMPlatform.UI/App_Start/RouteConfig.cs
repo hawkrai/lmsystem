5b458138a3a7f5362184791761cd116f141efcb4
Siarhei Piatrashka <sergei.petrashko8@gmail.com>
2020-04-05 19:03:20 +0300

    Fix profile error. RouteConfig formatting.

--- LMPlatform.UI/App_Start/RouteConfig.cs
+++ LMPlatform.UI/App_Start/RouteConfig.cs
@@ -1,37 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace LMPlatform.UI
 {
-    using System.ServiceModel.Activation;
-
-    using LMPlatform.UI.Services.News;
-
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.IgnoreRoute("Views/{file}.html");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("Views/{file}.html");
 
 			routes.MapRoute(
-				name: "Profile",
-				url: "Lms/{userLogin}",
-				defaults: new { controller = "Lms", action = "Index"});
+				"Profile",
+				"Lms/{userLogin}",
+				new {controller = "Lms", action = "Index"});
 
-			//routes.MapRoute(
-			//	name: "ProfilePage",
-			//	url: "Profile/Page/{userName}",
-			//	defaults: new { controller = "Profile", action = "Page" });
+			routes.MapRoute(
+				"ProfilePage",
+				"Profile/Page/{userLogin}",
+				new {controller = "Profile", action = "Page"});
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
-        }
-    }
+			routes.MapRoute(
+				"Default",
+				"{controller}/{action}/{id}",
+				new {controller = "Home", action = "Index", id = UrlParameter.Optional});
+		}
+	}
 }
