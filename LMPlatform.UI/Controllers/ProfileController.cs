fba1e8516712c75efd7d482c3b1aa26173567faf
Siarhei Piatrashka <sergei.petrashko8@gmail.com>
2020-04-05 18:41:50 +0300

    ProfileController (just formatting)

--- LMPlatform.UI/Controllers/ProfileController.cs
+++ LMPlatform.UI/Controllers/ProfileController.cs
@@ -12,54 +12,40 @@ using LMPlatform.UI.ViewModels.LmsViewModels;
 
 namespace LMPlatform.UI.Controllers
 {
-    using System.Collections.Generic;
-
-    using Application.Core;
-    using Application.Infrastructure.DPManagement;
-    using Application.Infrastructure.ProjectManagement;
-    using Application.Infrastructure.SubjectManagement;
-
-    using LMPlatform.UI.ViewModels.AdministrationViewModels;
-
-    using WebMatrix.WebData;
+	using System.Collections.Generic;
+	using Application.Core;
+	using Application.Infrastructure.DPManagement;
+	using Application.Infrastructure.ProjectManagement;
+	using Application.Infrastructure.SubjectManagement;
+	using ViewModels.AdministrationViewModels;
+	using WebMatrix.WebData;
 	using Application.Infrastructure.CPManagement;
 
 
 	public class ProfileController : BasicController
 	{
-		[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here.")]
-		private readonly LazyDependency<ICPManagementService> _cpManagementService = new LazyDependency<ICPManagementService>();
+		[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation",
+			Justification = "Reviewed. Suppression is OK here.")]
+		private readonly LazyDependency<ICPManagementService> _cpManagementService =
+			new LazyDependency<ICPManagementService>();
 
-		private ICPManagementService CpManagementService
-		{
-			get { return _cpManagementService.Value; }
-		}
+		private ICPManagementService CpManagementService => this._cpManagementService.Value;
 
-        private IDpManagementService DpManagementService
-        {
-            get { return _diplomProjectManagementService.Value; }
-        }
+		private IDpManagementService DpManagementService => this._diplomProjectManagementService.Value;
 
-        private readonly LazyDependency<IDpManagementService> _diplomProjectManagementService = new LazyDependency<IDpManagementService>();
+		private readonly LazyDependency<IDpManagementService> _diplomProjectManagementService =
+			new LazyDependency<IDpManagementService>();
 
-        private IProjectManagementService ProjectManagementService
-        {
-            get { return projectManagementService.Value; }
-        }
+		private IProjectManagementService ProjectManagementService => this.projectManagementService.Value;
 
-        private readonly LazyDependency<IProjectManagementService> projectManagementService = new LazyDependency<IProjectManagementService>();
+		private readonly LazyDependency<IProjectManagementService> projectManagementService =
+			new LazyDependency<IProjectManagementService>();
 
-		public IUsersManagementService UsersManagementService
-		{
-			get
-			{
-				return ApplicationService<IUsersManagementService>();
-			}
-		}
+		public IUsersManagementService UsersManagementService => this.ApplicationService<IUsersManagementService>();
 
 		public ActionResult Page(string userLogin = "")
 		{
-			if (User.IsInRole("lector") || User.IsInRole("student"))
+			if (this.User.IsInRole("lector") || this.User.IsInRole("student"))
 			{
 				var user = this.UsersManagementService.GetUser(userLogin);
 
@@ -67,60 +53,61 @@ namespace LMPlatform.UI.Controllers
 
 				model.UserActivity = new UserActivityViewModel();
 
-				ViewBag.ShowDpSectionForUser = DpManagementService.ShowDpSectionForUser(WebSecurity.CurrentUserId);
+				this.ViewBag.ShowDpSectionForUser =
+					this.DpManagementService.ShowDpSectionForUser(WebSecurity.CurrentUserId);
 
-				ViewData["userName"] = string.IsNullOrEmpty(userLogin) || WebSecurity.CurrentUserName == userLogin ? WebSecurity.CurrentUserName : userLogin;
+				this.ViewData["userName"] = string.IsNullOrEmpty(userLogin) || WebSecurity.CurrentUserName == userLogin
+					? WebSecurity.CurrentUserName
+					: userLogin;
 
-				return View(model);
+				return this.View(model);
 			}
-			else if (User.IsInRole("admin"))
+			else if (this.User.IsInRole("admin"))
 			{
-				var model = new LmsViewModel(WebSecurity.GetUserId(userLogin), User.IsInRole("lector"));
+				var model = new LmsViewModel(WebSecurity.GetUserId(userLogin), this.User.IsInRole("lector"));
 				model.UserActivity = new UserActivityViewModel();
-				ViewData["userName"] = string.IsNullOrEmpty(userLogin) || WebSecurity.CurrentUserName == userLogin ? WebSecurity.CurrentUserName : userLogin;
-				return View(model);
+				this.ViewData["userName"] = string.IsNullOrEmpty(userLogin) || WebSecurity.CurrentUserName == userLogin
+					? WebSecurity.CurrentUserName
+					: userLogin;
+				return this.View(model);
 			}
 
 
-			return RedirectToAction("Login", "Account");
+			return this.RedirectToAction("Login", "Account");
 		}
 
-        [HttpPost]
-        public ActionResult GetProfileStatistic(string userLogin)
-        {
-            var userService = new UsersManagementService();
-
-            var subjectService = new SubjectManagementService();
-
-            var user = userService.GetUser(userLogin);
-
-            var labs = 0;
-            var lects = 0;
-
-            if (user.Lecturer == null)
-            {
-                labs = subjectService.LabsCountByStudent(user.Id);
-            }
-            else
-            {
-                labs = subjectService.LabsCountByLector(user.Id);
-            }
-
-            return Json(new
-            {
-                Labs = labs,
-                Lects = lects
-            });
-        }
-
 		[HttpPost]
-        public ActionResult GetProfileInfoCalendar(string userLogin)
+		public ActionResult GetProfileStatistic(string userLogin)
 		{
 			var userService = new UsersManagementService();
 
-            var subjectService = new SubjectManagementService();
-           
-            var user = userService.GetUser(userLogin);
+			var subjectService = new SubjectManagementService();
+
+			var user = userService.GetUser(userLogin);
+
+			var labs = 0;
+			var lects = 0;
+
+			if (user.Lecturer == null)
+				labs = subjectService.LabsCountByStudent(user.Id);
+			else
+				labs = subjectService.LabsCountByLector(user.Id);
+
+			return this.Json(new
+			{
+				Labs = labs,
+				Lects = lects
+			});
+		}
+
+		[HttpPost]
+		public ActionResult GetProfileInfoCalendar(string userLogin)
+		{
+			var userService = new UsersManagementService();
+
+			var subjectService = new SubjectManagementService();
+
+			var user = userService.GetUser(userLogin);
 
 			var labsEvents = new List<ProfileCalendarViewModel>();
 			var lectEvents = new List<ProfileCalendarViewModel>();
@@ -128,143 +115,139 @@ namespace LMPlatform.UI.Controllers
 			if (user.Lecturer == null)
 			{
 				labsEvents = subjectService.GetGroupsLabEvents(user.Student.GroupId, user.Id).Select(
-					e => new ProfileCalendarViewModel() { color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId }).ToList();
+					e => new ProfileCalendarViewModel()
+						{color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId}).ToList();
 
 				lectEvents =
-				subjectService.GetLecturesEvents(user.Student.GroupId, user.Id)
-					.Select(e => new ProfileCalendarViewModel() { color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId })
-					.ToList();
+					subjectService.GetLecturesEvents(user.Student.GroupId, user.Id)
+						.Select(e => new ProfileCalendarViewModel()
+							{color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId})
+						.ToList();
 			}
 			else
 			{
 				labsEvents =
 					subjectService.GetLabEvents(user.Id)
-								.Select(e => new ProfileCalendarViewModel() { color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId })
-								.ToList();
+						.Select(e => new ProfileCalendarViewModel()
+							{color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId})
+						.ToList();
 
 				lectEvents =
-				subjectService.GetLecturesEvents(user.Id)
-					.Select(e => new ProfileCalendarViewModel() { color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId })
-					.ToList();
-			}            
+					subjectService.GetLecturesEvents(user.Id)
+						.Select(e => new ProfileCalendarViewModel()
+							{color = e.Color, title = e.Title, start = e.Start, subjectId = e.SubjectId})
+						.ToList();
+			}
 
-			return Json(new
-			                {
-			                    Labs = labsEvents,
-                                Lect = lectEvents
-			                });
+			return this.Json(new
+			{
+				Labs = labsEvents,
+				Lect = lectEvents
+			});
 		}
 
-        [HttpPost]
-        public ActionResult GetProfileInfoSubjects(string userLogin)
-        {
-            var userService = new UsersManagementService();
+		[HttpPost]
+		public ActionResult GetProfileInfoSubjects(string userLogin)
+		{
+			var userService = new UsersManagementService();
 
-            var subjectService = new SubjectManagementService();
+			var subjectService = new SubjectManagementService();
 
-            var user = userService.GetUser(userLogin);
+			var user = userService.GetUser(userLogin);
 
-            List<Subject> model;
+			List<Subject> model;
 
-            if (user.Lecturer == null)
-            {
-                model = subjectService.GetSubjectsByStudent(user.Id);
-            }
-            else
-            {
-                model = subjectService.GetSubjectsByLector(user.Id);
-            }
+			if (user.Lecturer == null)
+				model = subjectService.GetSubjectsByStudent(user.Id);
+			else
+				model = subjectService.GetSubjectsByLector(user.Id);
 
 
 			var returnModel = new List<object>();
 
-	        foreach (var subject in model)
-	        {
-		        returnModel.Add(new
-                                              {
-                                                  Name = subject.Name,
-                                                  Id = subject.Id,
-                                                  ShortName = subject.ShortName,
-												  Color = subject.Color,
-												  Completing = subjectService.GetSubjectCompleting(subject.Id, user.Lecturer != null ? "L" : "S", user.Student)
-                                              });
-	        }
+			foreach (var subject in model)
+				returnModel.Add(new
+				{
+					Name = subject.Name,
+					Id = subject.Id,
+					ShortName = subject.ShortName,
+					Color = subject.Color,
+					Completing = subjectService.GetSubjectCompleting(subject.Id, user.Lecturer != null ? "L" : "S",
+						user.Student)
+				});
 
-            return Json(returnModel);
-        }
+			return this.Json(returnModel);
+		}
 
-        [HttpPost]
-        public ActionResult GetProfileInfo(string userLogin)
-        {
-            var model = new ProfileVewModel();
+		[HttpPost]
+		public ActionResult GetProfileInfo(string userLogin)
+		{
+			var model = new ProfileVewModel();
 
-            var service = new UsersManagementService();
+			var service = new UsersManagementService();
 
-            var user = service.GetUser(userLogin);
+			var user = service.GetUser(userLogin);
 
-            model.UserType = user.Lecturer != null ? "1" : "2";
-            model.Avatar = user.Avatar;
-            model.SkypeContact = user.SkypeContact;
-            model.Email = user.Email;
-            model.Phone = user.Phone;
-            model.About = user.About;
-            model.LastLogitData = user.AttendanceList.LastOrDefault().ToString("dd/MM/yyyy hh:mm:ss");
-            if (user.Lecturer != null)
-            {
+			model.UserType = user.Lecturer != null ? "1" : "2";
+			model.Avatar = user.Avatar;
+			model.SkypeContact = user.SkypeContact;
+			model.Email = user.Email;
+			model.Phone = user.Phone;
+			model.About = user.About;
+			model.LastLogitData = user.AttendanceList.LastOrDefault().ToString("dd/MM/yyyy hh:mm:ss");
+			if (user.Lecturer != null)
+			{
 				model.Name = user.Lecturer.LastName + " " + user.Lecturer.FirstName + " " + user.Lecturer.MiddleName;
-                model.Skill = user.Lecturer.Skill;
-            }
-            else
-            {
+				model.Skill = user.Lecturer.Skill;
+			}
+			else
+			{
 				model.Name = user.Student.LastName + " " + user.Student.FirstName + " " + user.Student.MiddleName;
-                var course = int.Parse(DateTime.Now.Year.ToString()) - int.Parse(user.Student.Group.StartYear);
-                if (DateTime.Now.Month >= 9)
-                {
-                    course += 1;
-                }
+				var course = int.Parse(DateTime.Now.Year.ToString()) - int.Parse(user.Student.Group.StartYear);
+				if (DateTime.Now.Month >= 9) course += 1;
 
-                model.Skill = course > 5 ? "Окончил (-а)" : course + " курс";
+				model.Skill = course > 5 ? "Окончил (-а)" : course + " курс";
 
-                model.Group = user.Student.Group.Name;
+				model.Group = user.Student.Group.Name;
 				model.GroupId = user.Student.Group.Id;
-            }
+			}
 
 
-            return Json(model);
-        }
+			return this.Json(model);
+		}
 
-        [HttpPost]
-        public ActionResult GetUserProject(string userLogin)
-        {
-            var service = new UsersManagementService();
+		[HttpPost]
+		public ActionResult GetUserProject(string userLogin)
+		{
+			var service = new UsersManagementService();
 
-            var user = service.GetUser(userLogin);
-            var project = ProjectManagementService.GetProjectsOfUser(user.Id);
+			var user = service.GetUser(userLogin);
+			var project = this.ProjectManagementService.GetProjectsOfUser(user.Id);
 
-            return Json(project.Select(e => new
-                                                {
-                                                    Name = e.Project.Title
-                                                }));
-        }
+			return this.Json(project.Select(e => new
+			{
+				Name = e.Project.Title
+			}));
+		}
 
 
-        //[HttpPost]
-        //public ActionResult GetStudentAttendance(string userLogin)
-        //{
-        //    var service = new UsersManagementService();
+		//[HttpPost]
+		//public ActionResult GetStudentAttendance(string userLogin)
+		//{
+		//    var service = new UsersManagementService();
 
-        //    var user = service.GetUser(userLogin);
+		//    var user = service.GetUser(userLogin);
 
-        //    if (user.Lecturer != null)
-        //    {
-        //        return null;
-        //    }
+		//    if (user.Lecturer != null)
+		//    {
+		//        return null;
+		//    }
 
-        //    var subjectService = new SubjectManagementService();
+		//    var subjectService = new SubjectManagementService();
 
-        //    var attendance = subjectService.StudentAttendance(user.Id);
-        //}
-        
+		//    var attendance = subjectService.StudentAttendance(user.Id);
+		//}
+
 		[HttpGet]
 		public ActionResult GetAccessCode()
 		{
@@ -272,7 +255,7 @@ namespace LMPlatform.UI.Controllers
 
 			var code = repository.GetAll().OrderBy(e => e.Id).ToList().LastOrDefault();
 
-			return Json(code != null ? code.Number : string.Empty, JsonRequestBehavior.AllowGet);
+			return this.Json(code != null ? code.Number : string.Empty, JsonRequestBehavior.AllowGet);
 		}
 
 		[HttpGet]
@@ -283,13 +266,12 @@ namespace LMPlatform.UI.Controllers
 			var model = new AccessCode()
 			{
 				Date = DateTime.Now,
-				Number = RandomString(10,false)
-
+				Number = this.RandomString(10, false)
 			};
 
 			repository.Save(model);
 
-			return Json(model.Number, JsonRequestBehavior.AllowGet);
+			return this.Json(model.Number, JsonRequestBehavior.AllowGet);
 		}
 
 		/// <summary>
@@ -300,14 +282,15 @@ namespace LMPlatform.UI.Controllers
 		/// <returns>Random string</returns>
 		private string RandomString(int size, bool lowerCase)
 		{
-			StringBuilder builder = new StringBuilder();
-			Random random = new Random();
+			var builder = new StringBuilder();
+			var random = new Random();
 			char ch;
-			for (int i = 1; i < size + 1; i++)
+			for (var i = 1; i < size + 1; i++)
 			{
 				ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
 				builder.Append(ch);
 			}
+
 			if (lowerCase)
 				return builder.ToString().ToLower();
 			else
@@ -323,18 +306,14 @@ namespace LMPlatform.UI.Controllers
 
 			var user = service.GetUser(userLogin);
 
-			var news  = new List<SubjectNews>();
+			var news = new List<SubjectNews>();
 
 			if (user.Lecturer != null)
-			{
 				news = subjectService.GetNewsByLector(user.Id);
-			}
 			else
-			{
 				news = subjectService.GetNewsByGroup(user.Student.GroupId);
-			}
 
-			return Json(news.OrderByDescending(e => e.EditDate).ToList());
+			return this.Json(news.OrderByDescending(e => e.EditDate).ToList());
 		}
 
 		[HttpPost]
@@ -348,15 +327,15 @@ namespace LMPlatform.UI.Controllers
 
 			var labsEvents =
 				subjectService.GetLabEvents(user.Id)
-					.Select(e => new ProfileCalendarViewModel() { color = e.Color, title = e.Title, start = e.Start })
+					.Select(e => new ProfileCalendarViewModel() {color = e.Color, title = e.Title, start = e.Start})
 					.ToList();
 
 			var lectEvents =
 				subjectService.GetLecturesEvents(user.Id)
-					.Select(e => new ProfileCalendarViewModel() { color = e.Color, title = e.Title, start = e.Start })
+					.Select(e => new ProfileCalendarViewModel() {color = e.Color, title = e.Title, start = e.Start})
 					.ToList();
 
-			return Json(new
+			return this.Json(new
 			{
 				Labs = labsEvents,
 				Lect = lectEvents
